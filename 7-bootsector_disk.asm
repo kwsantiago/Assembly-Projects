ce3f43ae2e880d83f096af3bff086edf84e4f95b
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-04-09 15:43:38 -0400

    Create 7-bootsector_disk.asm

---
+++ 7-bootsector_disk.asm
@@ -0,0 +1,37 @@
+disk_load:
+    pusha
+    push dx
+
+    mov ah, 0x02 ; BIOS routine to read disk
+    mov al, dh   ; # of sectors to read
+    mov cl, 0x02  
+    mov ch, 0x00 
+    mov dh, 0x00 
+
+    int 0x13      ; BIOS interrupt
+    jc disk_error ; error handling 
+
+    pop dx
+    cmp al, dh    ; # of sectors read
+    jne sectors_error ; error handling
+    popa
+    ret
+
+
+disk_error:
+    mov bx, DISK_ERROR
+    call print
+    call print_newline
+    mov dh, ah
+    call print_hex 
+    jmp disk_loop
+
+sectors_error:
+    mov bx, SECTORS_ERROR
+    call print
+
+disk_loop:
+    jmp $
+
+DISK_ERROR: db "Disk read error", 0
+SECTORS_ERROR: db "Incorrect number of sectors read", 0
