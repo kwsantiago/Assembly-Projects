7a4bde27a590381923860d0022f266a6e48bc35a
Kyle Santiago <kwsantiago@mail.usf.edu>
2020-04-09 18:32:26 -0400

    Update 5-print.asm

--- 5 - Print with Hex/5-print.asm
+++ 5 - Print with Hex/5-print.asm
@@ -2,28 +2,29 @@ print:
     pusha
 
 start:
-    mov al, [bx] ; bx is the base address of the string
-    cmp al, 0
+    mov al, [bx] ; 'bx' is the base address for the string
+    cmp al, 0 
     je done
 
-    mov ah, 0x0e ; teletype BIOS print routine
-    int 0x10 ; print
+    mov ah, 0x0e
+    int 0x10 ; 'al' already contains the char
 
-    ; increment pointer
+    ; increment pointer and do next loop
     add bx, 1
     jmp start
 
 done:
     popa
-    return
+    ret
 
-print_newline:
+print_nl:
     pusha
+    
     mov ah, 0x0e
     mov al, 0x0a ; newline char
     int 0x10
     mov al, 0x0d ; carriage return
     int 0x10
-
+    
     popa
     ret
